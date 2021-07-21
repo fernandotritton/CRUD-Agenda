24df56a41a230e6747b01e337609475252eb44c0
CRUD-Agenda <parraflt@gmail.com>
2021-07-21 14:03:39 -0400

    project

---
+++ dataagenda.js
@@ -0,0 +1,151 @@
+
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.scheduleData = [
+        {
+            Id: 1,
+            Subject: 'Brendfort vs Arsenal',
+            StartTime: new Date(2021, 1, 11, 9, 30),
+            EndTime: new Date(2021, 1, 11, 11, 0),
+            CategoryColor: '#1aaa55'
+        }, {
+            Id: 2,
+            Subject: 'Euro Cup 2020',
+            StartTime: new Date(2021, 1, 12, 12, 0),
+            EndTime: new Date(2021, 1, 12, 14, 0),
+            CategoryColor: '#357cd2'
+        }, {
+            Id: 3,
+            Subject: 'Bit Coin Crash',
+            StartTime: new Date(2021, 1, 13, 9, 30),
+            EndTime: new Date(2021, 1, 13, 11, 0),
+            CategoryColor: '#7fa900'
+        }, {
+            Id: 4,
+            Subject: 'Space X lauch',
+            StartTime: new Date(2021, 1, 14, 13, 0),
+            EndTime: new Date(2021, 1, 14, 14, 30),
+            CategoryColor: '#ea7a57'
+        }, {
+            Id: 5,
+            Subject: 'Tesla Unveil new model X',
+            StartTime: new Date(2021, 1, 15, 12, 0),
+            EndTime: new Date(2021, 1, 15, 14, 0),
+            CategoryColor: '#00bdae'
+        }, {
+            Id: 6,
+            Subject: 'Mysteries of Bermuda Triangle',
+            StartTime: new Date(2021, 1, 15, 9, 30),
+            EndTime: new Date(2021, 1, 15, 11, 0),
+            CategoryColor: '#f57f17'
+        }, {
+            Id: 7,
+            Subject: 'Glaciers and Snowflakes',
+            StartTime: new Date(2021, 1, 16, 11, 0),
+            EndTime: new Date(2021, 1, 16, 12, 30),
+            CategoryColor: '#1aaa55'
+        }, {
+            Id: 8,
+            Subject: 'New Cardano Project',
+            StartTime: new Date(2021, 1, 17, 9, 0),
+            EndTime: new Date(2021, 1, 17, 10, 0),
+            CategoryColor: '#357cd2'
+        }, {
+            Id: 9,
+            Subject: 'Dubai Expo',
+            StartTime: new Date(2021, 1, 19, 11, 0),
+            EndTime: new Date(2021, 1, 19, 13, 0),
+            CategoryColor: '#7fa900'
+        }, {
+            Id: 10,
+            Subject: 'EuroVision Festival',
+            StartTime: new Date(2021, 1, 21, 11, 0),
+            EndTime: new Date(2021, 1, 21, 13, 0),
+            CategoryColor: '#ea7a57'
+        }, 
+    ]  
+
+    exports.resourceData = [
+        {
+            Id: 1,
+            Subject: 'Workflow Analysis',
+            StartTime: new Date(2021, 3, 1, 9, 30),
+            EndTime: new Date(2021, 3, 1, 12, 0),
+            IsAllDay: false,
+            OwnerId: 1,
+            RoomId: 1
+        }, {
+            Id: 2,
+            Subject: 'Requirement planning',
+            StartTime: new Date(2021, 3, 1, 12, 30),
+            EndTime: new Date(2021, 3, 1, 14, 45),
+            IsAllDay: false,
+            OwnerId: 2,
+            RoomId: 2
+        }, {
+            Id: 3,
+            Subject: 'Quality Analysis',
+            StartTime: new Date(2021, 3, 2, 10, 0),
+            EndTime: new Date(2021, 3, 2, 12, 30),
+            IsAllDay: false,
+            OwnerId: 1,
+            RoomId: 1
+        }, {
+            Id: 4,
+            Subject: 'Resource planning',
+            StartTime: new Date(2021, 3, 2, 13, 0),
+            EndTime: new Date(2021, 3, 2, 15, 30),
+            IsAllDay: false,
+            OwnerId: 2,
+            RoomId: 2
+        }, {
+            Id: 5,
+            Subject: 'Timeline estimation',
+            StartTime: new Date(2021, 3, 3, 9, 0),
+            EndTime: new Date(2021, 3, 3, 11, 30),
+            IsAllDay: false,
+            OwnerId: 1,
+            RoomId: 1
+        }, {
+            Id: 6,
+            Subject: 'Developers Meeting',
+            StartTime: new Date(2021, 3, 3, 14, 0),
+            EndTime: new Date(2021, 3, 3, 16, 45),
+            IsAllDay: false,
+            OwnerId: 2,
+            RoomId: 2
+        }, {
+            Id: 7,
+            Subject: 'Project Review',
+            StartTime: new Date(2021, 3, 4, 11, 15),
+            EndTime: new Date(2021, 3, 4, 13, 30),
+            IsAllDay: false,
+            OwnerId: 1,
+            RoomId: 1
+        }, {
+            Id: 8,
+            Subject: 'Manual testing',
+            StartTime: new Date(2021, 3, 4, 9, 15),
+            EndTime: new Date(2021, 3, 4, 11, 45),
+            IsAllDay: false,
+            OwnerId: 2,
+            RoomId: 2
+        }, {
+            Id: 9,
+            Subject: 'Project Preview',
+            StartTime: new Date(2021, 3, 5, 9, 30),
+            EndTime: new Date(2021, 3, 5, 12, 45),
+            IsAllDay: false,
+            OwnerId: 1,
+            RoomId: 1
+        }, {
+            Id: 10,
+            Subject: 'Cross-browser testing',
+            StartTime: new Date(2021, 3, 5, 13, 45),
+            EndTime: new Date(2021, 3, 5, 16, 30),
+            IsAllDay: false,
+            OwnerId: 2
+        }, 
+    ];
+});
